77e26620f3ca66028dbe379d9620e56a559fcfc2
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2024-08-13 17:30:04 +0530

    add mesh to home gltfpack intstall

--- dependency_install.sh
+++ dependency_install.sh
@@ -12,3 +12,11 @@ pip install -e .
 cd ../../../
 
 sudo apt install -y unzip nano 
+
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# download and install Node.js (you may need to restart the terminal)
+nvm install 20
+
+npm -i -g gltfpack
--- engine.sh
+++ engine.sh
@@ -12,3 +12,15 @@ source dependency_install.sh
 mkdir outputs/
 bash start_train.sh
 
+#mv mesh to home
+mkdir output_mesh
+cp -r output/refined_mesh/inputs/* output_mesh/. 
+
+#convert .obj to .glb
+gltfpack -i output_mesh/*.obj -o output_mesh/   model.glb
+#clean
+rm -r outputs/ output/ inputs/
+
+#copy frmo remote ssh to local
+#scp -P 49778 root@ip_address:~/SuGaR/output_mesh/model.glb
+
